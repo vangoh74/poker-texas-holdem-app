a67a694bc69b563e14a4bb5e2ec5a676d783c6e7
vangoh74 <vandicleiago@hotmail.com>
2022-05-22 21:05:50 +0200

    Refactor frontend configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from "./pages/Header";
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import TableItemsOverview from "./components/TableItemsOverview";
 import useTableItems from "./hooks/UseTableItems";
 
 export default function App() {
     const tableItems = useTableItems();
   return (
-    <div className="App">
+    <BrowserRouter>
         <Header />
         <Routes>
             <Route path="/"
                 element={<TableItemsOverview
                     tableItems={tableItems}/>} />
         </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
