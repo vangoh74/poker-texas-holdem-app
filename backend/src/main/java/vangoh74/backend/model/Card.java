9d81efb44a25dc361564a2e4f81a6a46b809c565
vangoh74 <vandicleiago@hotmail.com>
2022-05-22 15:27:29 +0200

    Refactor Card Model: Builder and NoArgsConstructor Anotations added.

--- backend/src/main/java/vangoh74/backend/model/Card.java
+++ backend/src/main/java/vangoh74/backend/model/Card.java
@@ -1,8 +1,12 @@
 package vangoh74.backend.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Card {
