64a2ac0d8953fc8a31a4b29fd954787cb5f0a977
vangoh74 <101346358+vangoh74@users.noreply.github.com>
2022-05-23 21:48:17 +0200

    Update frontend/src/components/Title.tsx
    Co-authored-by: Stefan Bollmann <stefan.bollmann@rwth-aachen.de>

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -4,7 +4,7 @@ import "./css/Title.css"
 export default function Title() {
     return (
         <div className={"titleContainer"}>
-            <h1> Capstone Texas Hold'em App </h1>
+            <h1>Texas Hold'em</h1>
         </div>
     )
 }
