6f21ea1d712500f05fa896f5c1669ad8c0648c41
vangoh74 <vandicleiago@hotmail.com>
2022-05-31 21:48:35 +0200

    Added SecurityConfig for WebSecurity (extends WebSecurityConfigurerAdapter)

---
+++ backend/src/main/java/vangoh74/backend/security/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package vangoh74.backend.security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
