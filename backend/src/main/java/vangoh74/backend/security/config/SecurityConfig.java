2d9946066039343e2a63d8a481671c604380397b
vangoh74 <vandicleiago@hotmail.com>
2022-05-31 23:05:18 +0200

    Added PasswordEncoder in SecurityConfig with Argon2PasswordEncoder: Object to encrypt password

--- backend/src/main/java/vangoh74/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/vangoh74/backend/security/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package vangoh74.backend.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import vangoh74.backend.security.service.AppUserDetailsService;
 
 @EnableWebSecurity
@@ -20,6 +24,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(appUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder();
     }
 
     @Override
