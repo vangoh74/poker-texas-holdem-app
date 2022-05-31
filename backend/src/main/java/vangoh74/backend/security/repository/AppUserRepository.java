3f67fdbcd56149f1c7ae483041b349ccd2749012
vangoh74 <vandicleiago@hotmail.com>
2022-05-31 22:12:58 +0200

    Extend Repo with findAppUserByUsername method

--- backend/src/main/java/vangoh74/backend/security/repository/AppUserRepository.java
+++ backend/src/main/java/vangoh74/backend/security/repository/AppUserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import vangoh74.backend.security.model.AppUser;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends MongoRepository<AppUser, String> {
+
+    Optional<AppUser> findAppUserByUsername(String username);
 }
