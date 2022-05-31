a36d65aeb34280e9ee3f8af8a1f865f18a68f5c8
vangoh74 <vandicleiago@hotmail.com>
2022-05-31 23:38:11 +0200

    Added AppUserDto in package dto, to return only the username in response to protect the password

---
+++ backend/src/main/java/vangoh74/backend/dto/AppUserDto.java
@@ -0,0 +1,14 @@
+package vangoh74.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AppUserDto {
+    private String username;
+}
