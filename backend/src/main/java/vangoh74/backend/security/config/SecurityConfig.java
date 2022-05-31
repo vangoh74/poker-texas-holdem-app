0ab6cbbdea90cd29bce9f2c5915863c7efe0adcc
vangoh74 <vandicleiago@hotmail.com>
2022-05-31 22:27:08 +0200

    Added in SecurityConfig configure method with authentication manager builder. (SecretObject is built in Spring Authentication provider) This will give out all users

--- backend/src/main/java/vangoh74/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/vangoh74/backend/security/config/SecurityConfig.java
@@ -1,13 +1,26 @@
 package vangoh74.backend.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import vangoh74.backend.security.service.AppUserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserDetailsService appUserDetailsService;
+
+    @Autowired
+    public SecurityConfig(AppUserDetailsService appUserDetailsService) {
+        this.appUserDetailsService = appUserDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsService);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
