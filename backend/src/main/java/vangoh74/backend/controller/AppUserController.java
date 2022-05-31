25bbd1a480453e86d66835b50fb1b6306d0f0763
vangoh74 <vandicleiago@hotmail.com>
2022-05-31 23:50:14 +0200

    Added AppUserController with Requestmapping Api/user/me, to get the current user (Principal)

---
+++ backend/src/main/java/vangoh74/backend/controller/AppUserController.java
@@ -0,0 +1,19 @@
+package vangoh74.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vangoh74.backend.dto.AppUserDto;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/user/")
+public class AppUserController {
+
+    @GetMapping("me")
+    public AppUserDto getLoggedInUser(Principal principal) {
+        return new AppUserDto(principal.getName());
+    }
+}
