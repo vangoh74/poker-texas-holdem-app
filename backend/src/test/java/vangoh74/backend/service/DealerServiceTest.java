24321539c5c10afb9f0bd934a8f901c4a0323ea2
vangoh74 <vandicleiago@hotmail.com>
2022-06-01 16:02:39 +0200

    Refactor DealerServiceTest

--- backend/src/test/java/vangoh74/backend/service/DealerServiceTest.java
+++ backend/src/test/java/vangoh74/backend/service/DealerServiceTest.java
@@ -41,12 +41,12 @@ class DealerServiceTest {
 
         // WHEN
         Card actual = dealerService.deal();
-        //int actualSize = deckCards.getPokerDeck().size();
+        int actualSize = deckCards.getPokerDeck().size();
 
         // THEN
         Card expected = card_1;
-       // int expectedSize = 3;
+        int expectedSize = 3;
         assertEquals(expected, actual);
-       // assertEquals(expectedSize, actualSize);
+        assertEquals(expectedSize, actualSize);
     }
 }
