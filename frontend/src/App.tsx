84f5f10046f1243713e39afcb234c3b9b9823774
vangoh74 <vandicleiago@hotmail.com>
2022-06-02 14:26:06 +0200

    Refactor App so that BrowserRouter is one level high (now in Index), allowing you to use the AuthProvider. Also, a new route has been added, that can only be used with authentication (RequireAuth Component)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,22 @@ import './App.css';
 import Header from "./components/Header";
 import {Route, Routes} from "react-router-dom";
 import TableItemsOverview from "./components/TableItemsOverview";
-import UseTableItems from "./hooks/useTableItems";
 import LoginPage from "./pages/LoginPage";
 import {ToastContainer} from "react-toastify";
+import RequireAuth from "./routing/RequireAuth";
 
 export default function App() {
-    const tableItems = UseTableItems();
 
   return (
     <>
         <ToastContainer />
         <Header />
         <Routes>
-            <Route path="/"
-                   element={<TableItemsOverview
-                       tableItems={tableItems} />}
-            />
+            <Route element={<RequireAuth />}>
+                <Route path="/"
+                       element={<TableItemsOverview />}
+                />
+            </Route>
             <Route path={"/login"} element={<LoginPage />} />
         </Routes>
     </>
