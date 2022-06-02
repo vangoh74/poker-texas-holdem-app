572b2c2262c6493c6297518cad5c45a31c379045
vangoh74 <vandicleiago@hotmail.com>
2022-06-02 11:39:05 +0200

    Refactor App, AuthProvider, index und LoginPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import TableItemsOverview from "./components/TableItemsOverview";
 import UseTableItems from "./hooks/useTableItems";
 import LoginPage from "./pages/LoginPage";
+import {ToastContainer} from "react-toastify";
 
 export default function App() {
     const tableItems = UseTableItems();
+
   return (
-    <BrowserRouter>
+    <>
+        <ToastContainer />
         <Header />
         <Routes>
             <Route path="/"
-                element={<TableItemsOverview
-                    tableItems={tableItems}/>} />
+                   element={<TableItemsOverview
+                       tableItems={tableItems} />}
+            />
             <Route path={"/login"} element={<LoginPage />} />
         </Routes>
-    </BrowserRouter>
+    </>
   );
 }
-
-
