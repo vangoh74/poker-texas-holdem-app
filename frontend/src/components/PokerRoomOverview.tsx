5369e4562a99148b75aeac1d0f0f0310c5b65e8c
vangoh74 <vandicleiago@hotmail.com>
2022-06-05 20:30:49 +0200

    Added PokerRoomOverview with Style: this Component represents a gallery with all tables registered in the database.

--- frontend/src/components/PokerRoomOverview.tsx
+++ frontend/src/components/PokerRoomOverview.tsx
@@ -1,15 +1,20 @@
-import GalleryTable from "./GalleryTable";
+import Table from "./Table";
 import UseTableItems from "../hooks/useTableItems";
+import "./css/PokerRoomOverview.css"
 
 export default function PokerRoomOverview() {
 
     const tableItems = UseTableItems();
 
     return (
-        <div className={"tablesGallery"}>
-            {
-                tableItems.map( tableItem => <GalleryTable tableItem={tableItem} />)
-            }
+        <div className={"room-background"}>
+            <div className={"gallery"}>
+                {
+                    tableItems.map(tableItem =>
+                        <Table key={tableItem.id} tableItem={tableItem} />)
+                }
+            </div>
         </div>
+
     );
 }
