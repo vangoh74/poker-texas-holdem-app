b6f95e70dbe1fd4d836d21f72ec512032c20dbb0
vangoh74 <vandicleiago@hotmail.com>
2022-06-07 21:59:33 +0200

    Added HashRouter to Index in Frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from "./context/AuthProvider";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <AuthProvider>
               <App />
           </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
 
