91caba5ec754e4874a4b5ab6e0f422e7fadf6e5b
vangoh74 <vandicleiago@hotmail.com>
2022-06-07 02:15:50 +0200

    Added function to get table items by table id

--- frontend/src/service/api-service.ts
+++ frontend/src/service/api-service.ts
@@ -5,5 +5,9 @@ export const getAllTableItems: (token: string) =>
     Promise<TableItem[]> = (token) => {
         return axios.get("/api/tableitems", {headers: {"Authorization": token}})
             .then(response => response.data)
+    };
 
-    }
+export function getTableItemsBy(id: string) {
+    return axios.get(`/api/tableitems/${id}`)
+    .then(response => response.data)
+}
