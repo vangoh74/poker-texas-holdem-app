2145582086cbfc1aa7492e882f0f3cb17bafa0eb
vangoh74 <vandicleiago@hotmail.com>
2022-06-11 19:46:00 +0200

    Refactor Model TableItem

--- backend/src/main/java/vangoh74/backend/model/TableItem.java
+++ backend/src/main/java/vangoh74/backend/model/TableItem.java
@@ -18,12 +18,11 @@ public class TableItem {
 
     @Id
     private String id;
+    private int maxSize;
     private int roundNumber;
     private RoundState roundState;
     private int bigBlind;
     private int smallBlind;
-    private int maxSize;
-    private int freeSeats;
     private double tableChips;
     private List<Card> tableCards;
     private List<Player> players;
