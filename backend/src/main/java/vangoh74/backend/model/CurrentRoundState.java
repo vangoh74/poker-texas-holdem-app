0f28e81c1deae0416011dd38857cb38ed44f4136
vangoh74 <vandicleiago@hotmail.com>
2022-06-13 18:13:17 +0200

    Added CurrentRoundState Model to save the game logic steps

---
+++ backend/src/main/java/vangoh74/backend/model/CurrentRoundState.java
@@ -0,0 +1,16 @@
+package vangoh74.backend.model;
+
+public enum CurrentRoundState {
+
+    LIST_START,
+    LIST_END,
+    PRE_FLOP,
+    FLOP,
+    TURN,
+    PRE_TURN,
+    RIVER,
+    PRE_RIVER,
+    PRE_SHOWDOWN,
+    SHOWDOWN
+
+}
--- backend/src/main/java/vangoh74/backend/model/RoundState.java
+++
@@ -1,9 +0,0 @@
-package vangoh74.backend.model;
-
-public enum RoundState {
-
-    PRE_FLOP,
-    PRE_TURN,
-    PRE_RIVER
-
-}
